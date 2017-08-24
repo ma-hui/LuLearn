8070a5105a2b52ca9d6743c0e7784df397fff575
马灰 <912990012@qq.com>
2017-08-24 14:33:34 +0800

    lua 的require 和loadfile实际效果差不过，一个导入模块，一个载入文件。导入或载入后，都会先运行对应的文件或者模块。

---
+++ dofile and require/df.lua
@@ -0,0 +1,20 @@
+--use require
+require ("test")
+
+function Diff()
+	print ("Now in df.lua ")
+end
+
+Diff()
+Hello("I'm using require")
+
+
+-- --use dofile 
+-- dofile('test.lua')
+
+-- function Diff()
+-- 	print ("Now in df.lua ")
+-- end
+-- Diff()
+-- Hello("I'm using dofile")
+
